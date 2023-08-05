1db33fe48bb7f3825d6756d9919ac93092917c8c
TailWind76 <xaron.dokuchaev@yandex.ru>
2023-08-05 23:54:34 +0700

    setup

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const http = require('http');
+const WebSocket = require('ws');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('User connected');
+
+  ws.on('message', (message) => {
+    console.log('Received message:', message);
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('User disconnected');
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
